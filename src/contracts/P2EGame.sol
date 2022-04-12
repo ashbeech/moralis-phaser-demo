0f9219bfdb12b1bb5449fc114547bfc8022e3113
Ash Beech <ash@eatplants.cc>
2022-04-12 03:04:53 +0100

    .

--- src/contracts/P2EGame.sol
+++ src/contracts/P2EGame.sol
@@ -5,9 +5,10 @@ import "./GameToken.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract P2EGame is Ownable {
+  // admin address
   address admin;
+  // balance of tokens held in escrow
   uint256 public totalBalance;
-
   // this is the erc20 GameToken contract address
   address constant tokenAddress = 0x…; // <-- INSERT DEPLYED ERC20 TOKEN CONTRACT HERE
 
@@ -19,31 +20,34 @@ contract P2EGame is Ownable {
     bool locked;
     bool spent;
   }
-
+  // map game to balances
   mapping(address => mapping(uint256 => Game)) public balances;
 
+  // only admin account can unlock escrow
   modifier onlyAdmin {
     require(msg.sender == admin, "Only admin can unlock escrow.");
     _;
   }
 
-    /**
-     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
-     * account that deploys the contract.
-     *
-     * See {ERC20-constructor}.
-     */
-    constructor() {
-        admin = msg.sender;
-    }
+  /**
+    * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
+    * account that deploys the contract.
+    *
+    * See {ERC20-constructor}.
+    */
+  constructor() {
+      admin = msg.sender;
+  }
 
   // player startGames
   // staked tokens get moved to the escrow (this contract)
   function startGame(uint256 _gameId, address _treasury, uint256 _amount) external returns (uint256) {
     GameToken token = GameToken(tokenAddress);
     // approve contract to spend amount tokens
-    require(token.approve(address(this), _amount), "P2EGame: approval has failed");
+    //require(token.approve(address(this), _amount), "P2EGame: approval has failed"); // <-- this approval method doesn't work and player must approve token contract directly
+    // must include amount >1 token (1000000000000000000)
     require(_amount >= 1000000000000000000, "P2EGame: must insert 1 whole token");
+    // transfer from player to the contract's address to be locked in escrow
     token.transferFrom(msg.sender, address(this), _amount);
     
     totalBalance += _amount;
@@ -66,6 +70,7 @@ contract P2EGame is Ownable {
     balances[_player][_gameId].locked = false;
 
     GameToken token = GameToken(tokenAddress);
+    // transfer to player the value locked in escrow
     token.transfer(_player, balances[_player][_gameId].amount);
 
     totalBalance -= balances[_player][_gameId].amount;
@@ -76,6 +81,7 @@ contract P2EGame is Ownable {
   // admin sends funds to treasury if player loses game
   function playerLost(address _player, uint256 _gameId) onlyAdmin external returns(bool) {
     GameToken token = GameToken(tokenAddress);
+    // transfer to treasury the value locked in escrow
     token.transfer(balances[_player][_gameId].treasury, balances[_player][_gameId].amount);
 
     balances[_player][_gameId].spent = true;
@@ -83,12 +89,13 @@ contract P2EGame is Ownable {
     return true;
   }
 
-  // player is able to withdraw unlocked tokens
+  // player is able to withdraw unlocked tokens without admin if unlocked
   function withdraw(uint256 _gameId) external returns(bool) {
     require(balances[msg.sender][_gameId].locked == false, "This escrow is still locked");
     require(balances[msg.sender][_gameId].spent == false, "Already withdrawn");
 
     GameToken token = GameToken(tokenAddress);
+    // transfer to player of game (msg.sender) the value locked in escrow
     token.transfer(msg.sender, balances[msg.sender][_gameId].amount);
 
     totalBalance -= balances[msg.sender][_gameId].amount;
